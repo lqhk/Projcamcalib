3ded8f3fa2585d0026491554a795e01e915d6dc1
lqhk <liuhaoouc@gmail.com>
2013-10-13 20:41:04 +0800

    --Modified-- Unified rawphase matrix generation by using pre-calculated phase differences.

--- src/matlab_src/testCoorspond.m
+++ src/matlab_src/testCoorspond.m
@@ -27,7 +27,7 @@ phasediff24=brtimgs{2}-brtimgs{4};
 phasediff31=brtimgs{3}-brtimgs{1};
 rawbkgbrt=(brtimgs{1}+brtimgs{2}+brtimgs{3}+brtimgs{4})./4;
 rawgain=2.*realsqrt(realpow(phasediff24,2)+realpow(phasediff31,2))./(brtimgs{1}+brtimgs{2}+brtimgs{3}+brtimgs{4});
-rawphase=atan((brtimgs{2}-brtimgs{4})./(brtimgs{3}-brtimgs{1}));
+rawphase=atan(phasediff24./phasediff31);
 clear phasediff24 phasediff31;
 
 %% Estimate
