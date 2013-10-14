ec69c7867be077a77f37b450b52fdf819478759b
lqhk <liuhaoouc@gmail.com>
2013-10-14 23:08:35 +0800

    --Add-- Another itr-cost function for estimation. This new function is based on real matrix operations. The NaN elements will be ignored before final sum.

---
+++ src/matlab_src/itrIlluminationCost2.m
@@ -0,0 +1,39 @@
+% para=gain,phase,bkgbrt,cra,crb
+% mask is a 0 or 1 mask
+% brtimgsarray={brgimg1,brtimg2,brtimg3,brtimg4}
+function f = itrIlluminationCost2(para,mask,brtimgsarray)
+isdebug=0;
+imgsize=length(mask);
+sumresult=0;
+gain=para(1:imgsize);
+phase=para((imgsize+1):(2*imgsize));
+bkgbrt=para((2*imgsize+1):(3*imgsize));
+cra=para(3*imgsize+1);
+crb=para(3*imgsize+2);
+estI=cell([1,4]);
+sumI=cell([1,4]);
+for imgi=1:length(estI)
+    if isequal(imgi,4)
+        estI{imgi}=realpow(sin(phase),cra);
+    else
+        phasediff=imgi*pi/2;
+        estI{imgi}=realpow(sin(phase+phasediff),cra);
+    end
+    estI{imgi}=gain.*estI{imgi}+bkgbrt;
+    estI{imgi}=realpow(estI{imgi},crb);
+    sumI{imgi}=brtimgsarray{imgi}-estI{imgi};
+end
+for imgi=1:length(estI)
+    for pixi=1:imgsize;
+        if isnan(sumI{imgi}(pixi))
+            sumI{imgi}(pixi)=0;
+        end
+        if isequal(mask(pixi),0)
+            sumI{imgi}(pixi)=0;
+        end
+    end
+    sumI{imgi}=abs(sumI{imgi});
+    sumresult=sumresult+sum(sumI{imgi});
+end
+disp('looped');
+f=sumresult;
